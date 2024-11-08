958efdbe2734893bcada4eda036b862620bf658d
Robson Ribeiro <rib21006@byui.edu>
2024-11-08 18:29:55 -0300

    Primeiro commit

---
+++ SeçãoCSS/CSS intermediário e avançado/PHP/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php
+            Olá Mundo!
+        ?>
+    </h1>
+    <p>
+        Vamos tentar nos livrar da maldição
+    </p>
+</body>
+</html>
